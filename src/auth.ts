ce578618c0174dadcf0c6c66186c118888462c09
nbifrye <nbifrye@gmail.com>
2024-01-01 15:39:27 +0900

    redis json driver

--- src/auth.ts
+++ src/auth.ts
@@ -1,15 +1,17 @@
 import NextAuth from "next-auth";
 import { UnstorageAdapter } from "@/lib/unstorage-adapter";
 import { createStorage } from "unstorage";
-import fsDriver from "unstorage/drivers/fs";
+import redisJSONDriver from "@/lib/redis-json-driver";
 import authConfig from "@/auth.config";
 
 const storage = createStorage({
-  driver: fsDriver({ base: "./storage" }),
+  driver: redisJSONDriver({
+    username: "default",
+  }),
 });
 
 export const { handlers, auth } = NextAuth({
-  adapter: UnstorageAdapter(storage),
+  adapter: UnstorageAdapter(storage, { useItemRaw: true }),
   session: { strategy: "jwt" },
   ...authConfig,
 });
